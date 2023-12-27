8884f07447c20a7aa5135a41c01c954331742691
IshitaSrivast <20cs3034@rgipt.ac.in>
2023-12-28 03:39:12 +0530

    server swagger added

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const apiRoutes = require('./routes/apiRoutes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 // Initialize Express application
 const app = express();
@@ -12,7 +14,12 @@ app.use(cors());
 // HTTP request logging middleware for node.js
 app.use(morgan('tiny')); // 'tiny' gives minimal output
 
+const swaggerDocument = YAML.load('swagger.yaml');
+
 const port = 5000;
+
+// Swagger endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Mount the API routes on the '/api' path
 app.use('/api', apiRoutes);
