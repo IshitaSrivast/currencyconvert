9d702f612e582dd1ddbe20fdc4d8a020f4384c9e
IshitaSrivast <20cs3034@rgipt.ac.in>
2023-12-28 03:15:21 +0530

    client components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./header.scss";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+
+    
+
+
+return (
+    <>
+      <div className="header">
+   
+      </div>
+      <div className="line"> </div>
+    </>
+  );
+};
+
+export default Header;
