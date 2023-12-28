a190ac6584c530f9018949b710a63e50364ad280
IshitaSrivast <20cs3034@rgipt.ac.in>
2023-12-28 14:41:08 +0530

    header added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./header.scss";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+import logo from "./favicon.png";
 
 const Header = () => {
   return (
     <>
-      <div className="header"></div>
+      <div className="header">
+        <img src={logo}></img>
+      </div>
       <div className="line"> </div>
     </>
   );
